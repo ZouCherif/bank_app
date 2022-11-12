a1c410caaaccbbf6b3288baada5b03baeb3e632b
cherif <cheriffzouaoui@gmail.com>
2022-11-12 23:04:54 +0100

    business_Done

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { features } from "../constants"
+import styles, {layout} from "../style"
+import Button from './Button'
+
+const FeatureCard = ({icon, title, content, index}) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex flex-1 flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">{title}</h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">{content}</p>
+    </div>
+  </div>
+)
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do the buisiness,<br className="sm:block" /> we'll handle the money.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5:`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, quam, dolorum nesciunt voluptatibus facere laborum obcaecati itaque </p>
+        <Button styles="mt-10"/>  
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index}/>
+        ))}
+      </div>
+    </section>
   )
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({styles}) => {
   return (
-    <div>Button</div>
+    <button type='button' className={`font-poppins font-medium text-[18px] text-primary outline-none ${styles} py-4 px-6 bg-blue-gradient`}>
+      Get Started
+    </button>
   )
 }
 
